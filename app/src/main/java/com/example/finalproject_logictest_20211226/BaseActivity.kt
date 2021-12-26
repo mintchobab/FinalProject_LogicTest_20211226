d7b1b7b290d61d70256edf5f15082a59f5ece17f
seungyup <lsy9312242@naver.com>
2021-12-26 10:12:26 +0900

    Initial commit

---
+++ app/src/main/java/com/example/finalproject_logictest_20211226/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.example.finalproject_logictest_20211226
+
+import androidx.appcompat.app.AppCompatActivity
+
+// 상속 허용 방법 -> 1. Open,  2. abstract
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    
+
+}
---
+++ app/src/main/java/com/example/finalproject_logictest_20211226/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.finalproject_logictest_20211226
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.finalproject_logictest_20211226.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+    }
+}
