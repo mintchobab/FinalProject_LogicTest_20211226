7e98b09166988d17d368ac587189832aaf97ad68
seungyup <lsy9312242@naver.com>
2021-12-26 12:24:13 +0900

    보너스번호 검사 로직 추가

--- app/src/main/java/com/example/finalproject_logictest_20211226/LottoSimulatorActivity.kt
+++ app/src/main/java/com/example/finalproject_logictest_20211226/LottoSimulatorActivity.kt
@@ -90,12 +90,24 @@ class LottoSimulatorActivity : BaseActivity() {
 
             earnMoney += 2000000000  // 번 돈을 20억 증가
         } else if (correctCount == 5) {
-            rankCount3++
 
-            earnMoney += 1500000  // 번 돈을 50만원 증가
+//            보너스 번호 추가 검사.
+//            보너스번호가 => 내 숫자안에 들어있는가? 들어있다면, 맞춘 상황
+
+            if (myNumberList.contains(bonusNum)) {
+//                2등!
+                rankCount2++
+                earnMoney += 50000000  // 번 돈을 50만원 증가
+            }
+            else {
+//                3등!
+                rankCount3++
+                earnMoney += 1500000  // 번 돈을 50만원 증가
+            }
+
+
         } else if (correctCount == 4) {
             rankCount3++
-
             earnMoney += 50000  // 번 돈을 5만원 증가
         } else if (correctCount == 3) {
             rankCount5++
