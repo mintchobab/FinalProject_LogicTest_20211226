5c01126a15808bdd718d306383ae85d33a128bcb
seungyup <lsy9312242@naver.com>
2021-12-26 13:55:22 +0900

    리싸이클러 어댑터 붙이기

--- app/src/main/java/com/example/finalproject_logictest_20211226/NumberBaseballGameActivity.kt
+++ app/src/main/java/com/example/finalproject_logictest_20211226/NumberBaseballGameActivity.kt
@@ -2,7 +2,10 @@ package com.example.finalproject_logictest_20211226
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.BindingAdapter
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.finalproject_logictest_20211226.adapters.ChatAdapter
 import com.example.finalproject_logictest_20211226.databinding.ActivityNumberBaseballGameBinding
 import com.example.finalproject_logictest_20211226.datas.ChatData
 
@@ -11,6 +14,8 @@ class NumberBaseballGameActivity : BaseActivity() {
     lateinit var binding: ActivityNumberBaseballGameBinding
 
     val mChatList = ArrayList<ChatData>()
+
+    lateinit var mAdapter: ChatAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,5 +34,29 @@ class NumberBaseballGameActivity : BaseActivity() {
         mChatList.add(ChatData("CPU", "3자리 숫자로 문제가 생성되었습니다."))
         mChatList.add(ChatData("CPU", "밑의 입력칸을 이용해 3자리 숫자를 맞춰주세요."))
 
+        mAdapter = ChatAdapter(mContext, mChatList)
+        binding.chattingRecyclerView.adapter = mAdapter
+        binding.chattingRecyclerView.layoutManager = LinearLayoutManager(mContext)
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
