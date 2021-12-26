f6a25d202ca94c6fa453d63ff7bdda6a5652df35
seungyup <lsy9312242@naver.com>
2021-12-26 10:14:03 +0900

    BaseActivty 클래스 제작

--- app/src/main/java/com/example/finalproject_logictest_20211226/BaseActivity.kt
+++ app/src/main/java/com/example/finalproject_logictest_20211226/BaseActivity.kt
@@ -1,11 +1,22 @@
 package com.example.finalproject_logictest_20211226
 
+import android.content.Context
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 // 상속 허용 방법 -> 1. Open,  2. abstract
 
 abstract class BaseActivity : AppCompatActivity() {
 
-    
+    lateinit var mContext : Context
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mContext = this
+    }
+
+    abstract fun setupEvents()
+    abstract fun setupValues()
 
 }
